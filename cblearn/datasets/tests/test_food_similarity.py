3405fed58167b0f93de1be6b60ce8b87fd5701b0
Guillermo Aguilar <gmo.aguilar.c@gmail.com>
2026-08-24 17:18:26 +0200

    skip food similarity test

--- cblearn/datasets/tests/test_food_similarity.py
+++ cblearn/datasets/tests/test_food_similarity.py
@@ -3,7 +3,7 @@ import pytest
 
 from cblearn.datasets import fetch_food_similarity
 
-
+@pytest.mark.skip(reason="URL does not work")
 @pytest.mark.remote_data
 def test_fetch_food(tmp_path):
     data_home = tmp_path / 'cblearn_datasets'
